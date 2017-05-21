6a07cf865f08299dd5b244b460c67b0cf64852df
danngyw <danhoat@gmail.com>
2017-05-21 16:25:06 +0700

    uncommit

--- single-project.php
+++ single-project.php
@@ -41,25 +41,25 @@ global $wp_query;
                		<h3> <?php _e('Job details','boxtheme');?> </h3>
           			<?php the_content(); ?>
                     <?php
-	                    // $args = array(
-	                    //     'post_status' => 'any',
-	                    //     'post_type'   => 'attachment',
-	                    //     'post_parent' => $project->ID,
-	                    // );
-	                    // $att_query = new WP_Query( $args );
-	                    // if( $att_query-> have_posts() ){
-	                    //     echo '<p>';
-	                    //     echo '<h3>'.__('Files attach: ','boxtheme').'</h3>';
-	                    //     $files = array();
-	                    //     while ( $att_query-> have_posts()  ) {
-	                    //         global $post;
-	                    //         $att_query->the_post();
-	                    //         $feat_image_url = wp_get_attachment_url( $post->ID );
-	                    //         $files[] = '<span> <a href="'.$feat_image_url.'">'.get_the_title().'</a></span> ';
-	                    //     }
-	                    //     echo join(",",$files);
-	                    //     echo '</p>';
-	                    // }
+	                    $args = array(
+	                        'post_status' => 'any',
+	                        'post_type'   => 'attachment',
+	                        'post_parent' => $project->ID,
+	                    );
+	                    $att_query = new WP_Query( $args );
+	                    if( $att_query-> have_posts() ){
+	                        echo '<p>';
+	                        echo '<h3>'.__('Files attach: ','boxtheme').'</h3>';
+	                        $files = array();
+	                        while ( $att_query-> have_posts()  ) {
+	                            global $post;
+	                            $att_query->the_post();
+	                            $feat_image_url = wp_get_attachment_url( $post->ID );
+	                            $files[] = '<span> <a href="'.$feat_image_url.'">'.get_the_title().'</a></span> ';
+	                        }
+	                        echo join(",",$files);
+	                        echo '</p>';
+	                    }
                     ?>
 
           		</div> <!-- .col-md-8  Job details !-->
