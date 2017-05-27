53f137a50f942e7602d360677b5b900b33018546
danngyw <danhoat@gmail.com>
2017-05-27 20:51:13 +0700

    update

--- header.php
+++ header.php
@@ -67,35 +67,12 @@ global $user_ID;
 					<img src="<?php echo get_stylesheet_directory_uri().'/img/logo.png';?>" />
 				</a>
 			</div>
-			<div class="no-padding col-nav">
+			<div class="no-padding col-nav col-md-6">
 			<?php if ( has_nav_menu( 'top' ) ) { ?>
 				<?php get_template_part( 'template-parts/navigation', 'top' ); ?>
 			<?php } ?>
 			</div>
-			<div class="col-md-3 no-padding col-xs-12 col-search hide">
-				<?php
-					$project_link = $default =  get_post_type_archive_link(PROJECT);
-					$placeholder = __('Find a job','boxtheme');
-					$profile_link = get_post_type_archive_link(PROFILE);
-					if( is_post_type_archive(PROFILE) ){
-						$default = $profile_link;
-						$placeholder = __('Find a freelancer','boxtheme');
-					}
-				?>
-				<form class="frm-search" action="<?php echo $default;?>">
-					<span class="glyphicon glyphicon-search absolute search-icon"></span>
-					<select class="input-control absolute"  id="search_type">
-						<option value="<?php echo $project_link;?>" alt="<?php _e('Find a job','boxtheme');?>">
-							<?php _e('Job','boxtheme');?>
-						</option>
-						<option value="<?php echo $profile_link;?>" alt="<?php _e('Find a freelancer','boxtheme');?>">
-							<?php _e('Freelancer','boxtheme');?>
-						</option>
-					</select>
-					<input type="text" name="s" id="keyword" class="keyword full no-radius" value="<?php echo get_query_var('s');?>" placeholder="<?php echo $placeholder;?>" />
-					<button class="mobile-only" type="submit"><span class="glyphicon glyphicon-search"></span></button>
-				</form>
-			</div>
+			<!-- seach form here !-->
 
 			<div class="col-md-3 f-right align-right no-padding-left header-action">
 				<?php
