c7bcf0e0d890f2a84780d8b35f80f802d927ce59
danngyw <danhoat@gmail.com>
2017-06-02 00:19:59 +0700

    update

--- page-post-project.php
+++ page-post-project.php
@@ -7,13 +7,11 @@
 <div class="full-width">
 	<div class="container site-container">
 		<div class="row site-content" id="content" >
-			<div class="col-md-8 detail-project text-justify">
+			<div class="col-md-8 col-md-offset-2 detail-project text-justify">
 				<?php the_post(); ?>
 				<?php get_template_part( 'template-parts/project/form-post', 'project' );?>
 			</div>
-			<div class="col-md-4">
-				<?php get_sidebar('single');?>
-			</div>
+
 		</div>
 	</div>
 </div>
