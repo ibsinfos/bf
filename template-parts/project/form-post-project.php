0f88b068596cac67dc732e665fd82da4038a10f9
danngyw <danhoat@gmail.com>
2017-06-02 11:19:11 +0700

    update

--- template-parts/project/form-post-project.php
+++ template-parts/project/form-post-project.php
@@ -56,7 +56,7 @@
 	</div>
 	<div class="form-group ">
 	 	<div id="fileupload-container" class="file-uploader-area">
-		    <span class="btn btn-plain btn-file-uploader">
+		    <span class="btn btn-plain btn-file-uploader border-color">
 		      	<span class="fl-icon-plus"></span>
 		      	<input type="hidden" class="nonce_upload_field" name="nonce_upload_field" value="<?php echo wp_create_nonce( 'box_upload_file' ); ?>" />
 		      	<span id="file-upload-button-text " class="text-color"><span class="glyphicon glyphicon-plus text-color"></span> <?php _e('Upload Files','boxtheme');?></span>
