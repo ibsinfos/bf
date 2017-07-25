a2400ce477d474ad70938f511fd254af5ea61a31
danng <danhoat@gmail.com>
2017-07-25 21:52:54 +0700

    update

--- page-dashboard.php
+++ page-dashboard.php
@@ -9,16 +9,20 @@
 		<div class="row site-content" id="content" >
 			<div id="profile" class="col-md-12"> <!-- start left !-->
 				<ul  class="nav nav-pills" id ="heading_dashboard">
-					<li class="active"><a  href="#1a" data-toggle="tab">List project</a>			</li>
-					<li><a href="#tab_credit" data-toggle="tab">Credit</a>		</li>
+					<li class="active"><a  href="#1a" data-toggle="tab">ACTIVATE</a>			</li>
+					<li><a href="#tab_working" data-toggle="tab">WORKING</a>		</li>
+					<li><a href="#tab_done" data-toggle="tab">DONE</a></li>
 
 				</ul>
 				<div class="tab-content clearfix">
 					<div class="tab-pane active" id="1a">
 						<?php get_template_part( 'template-parts/dashboard/list', 'project' ); ?>
 					</div>
-			        <div class="tab-pane" id="tab_credit">
-			        <?php get_template_part( 'template-parts/dashboard/list', 'order' ); ?>
+			        <div class="tab-pane" id="tab_working">
+			       		<?php get_template_part( 'template-parts/dashboard/list', 'order' ); ?>
+					</div>
+					<div class="tab-pane" id="tab_done">
+			       		<?php get_template_part( 'template-parts/dashboard/list', 'order' ); ?>
 					</div>
 
 				</div>
