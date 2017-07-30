cb95f3f3c58e27b12af6df30af9954a2b64649ce
danng <danhoat@gmail.com>
2017-07-30 11:40:57 +0700

    update

--- sidebar/archive-profiles.php
+++ sidebar/archive-profiles.php
@@ -18,7 +18,7 @@
 	    </ul>
 	</div>
 	<div class="block full">
-		<h3> Countries <span class="toggle-check glyphicon  pull-right glyphicon-menu-down"></span></h3>
+		<h3> Skills <span class="toggle-check glyphicon  pull-right glyphicon-menu-down"></span></h3>
 
 	 	<ul class="list-checkbox ul-skills">
 
