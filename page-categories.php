928aaf02adcfa055cb65cf198252f0dcef62b31e
danng <danhoat@gmail.com>
2017-08-04 00:51:44 +0700

    update

--- page-categories.php
+++ page-categories.php
@@ -19,52 +19,64 @@ get_header();
 
 				<div class="col-md-12">
 					<?php
+						function box_list_categories($terms, $style = 1){
+
+							if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+								if($style == 1){
+									echo '<ul class="none-style">';
+									foreach ($terms as $key => $term) {
+										$term_link = get_term_link( $term );
+										echo '<li class="col-md-3"><a href="'. esc_url( $term_link ) .'">'.$term->name.'('.$term->count.')</a></li>';							}
+									echo '</ul>';
+								} else if($style == 2) {
+
+									$alphas=range("A","Z");
+									$text = array();
+									foreach($alphas as $char){
+										$text[$char] = array();
+									}
+
+								    foreach ( $terms as $term ) {
+								        $first_letter = strtoupper(substr( trim($term->name) , 0, 1) );
+								        if( isset($text[$first_letter]) )
+								        	array_push($text[$first_letter], $term);
+								    }
+
+								    foreach ($text as $key => $terms) {
+
+										if( !empty($terms) ){
+											echo '<div class="col-md-3"><ul class="none-style"><li class="cat-label"><label class="h5">'.$key.'</label></li>';
+											foreach ($terms as $term) {
+												$term_link = get_term_link( $term );
+												echo '<li><a href="'. esc_url( $term_link ) .'">'. $term->name .'</a></li>';
+											}
+											echo '</ul></div>';
+										}
+									}
+								}
+
+							} else {
+								echo 'List categories is empty';
+							}
+						}
+
 						$terms = get_terms( 'project_cat', array(
 						    'orderby'    => 'name',
 						    'hide_empty' => 0
 						) );
 
 
-						echo '<h2 class="col-md-12"> LIST CATEGORIES </h2>';
-						if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
-							echo '<div class="full">';
-							echo '<ul class="none-style">';
-							foreach ($terms as $key => $term) {
-								$term_link = get_term_link( $term );
-								echo '<li class="col-md-3"><a href="'. esc_url( $term_link ) .'">'.$term->name.'('.$term->count.')</a></li>';							}
-							echo '</ul>';
 
-							echo '</div> <!-- full !-->';
-							echo '<div class="full ">';
-							echo '<br />';
-							echo '<h2 class="col-md-12"> LIST CATEGORIES </h2>';
-							$alphas=range("A","Z");
-							$text = array();
-							foreach($alphas as $char){
-								$text[$char] = array();
-							}
 
-						    foreach ( $terms as $term ) {
-						        $first_letter = strtoupper(substr( trim($term->name) , 0, 1) );
-						        if( isset($text[$first_letter]) )
-						        	array_push($text[$first_letter], $term);
-						    }
+						echo '<div class="full">';
+							echo '<h2 class="col-md-12"> LIST CATEGORIES STYLE 1 </h2>';
+							box_list_categories($terms);
+						echo '</div> <!-- full !-->';
 
-						    foreach ($text as $key => $terms) {
-
-								if( !empty($terms) ){
-									echo '<div class="col-md-3"><ul class="none-style"><li class="cat-label"><label class="h5">'.$key.'</label></li>';
-									foreach ($terms as $term) {
-										$term_link = get_term_link( $term );
-										echo '<li><a href="'. esc_url( $term_link ) .'">'. $term->name .'</a></li>';
-									}
-									echo '</ul></div>';
-								}
-							}
-							echo '</div>';
-						}  else{
-							echo ' Empty';
-						}
+						echo '<div class="full ">';
+							echo '<h2 class="col-md-12"> LIST CATEGORIES STYLE 2</h2>';
+							box_list_categories($terms, $style = 2);
+						echo '</div>';
 
 
 					?>
