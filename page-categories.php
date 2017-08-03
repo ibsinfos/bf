62fc57bf34159d8be2c27eceb23f82e02bcf0afc
danng <danhoat@gmail.com>
2017-08-03 23:49:14 +0700

    update

--- page-categories.php
+++ page-categories.php
@@ -17,31 +17,56 @@ get_header();
 			<div class="row">
 				<div class="col-md-12">
 					<?php
-
-						$alphas=range("A","Z");
-						$text = array();
-						foreach($alphas as $char){
-							$text[$char] = array();
-						}
-
-						$terms = get_terms( 'project_cat' );
+						$terms = get_terms( 'project_cat', array(
+						    'orderby'    => 'name',
+						    'hide_empty' => 0
+						) );
+						echo '<div class="full">';
+						echo '<h2 class="col-md-12"> Style 1</h2>';
 						if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+
+							echo '<ul class="none-style">';
+							foreach ($terms as $key => $term) {
+								echo '<li class="col-md-3">'.$term->name.'</li>';
+							}
+							echo '</ul>';
+						} else{
+
+							echo ' Empty';
+						}
+						echo '</div> <!-- full !-->';
+
+						$terms = get_terms( 'project_cat',array( 'hide_empty' => 0) );
+
+						if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+
+							echo '<div class="full ">';
+							echo '<h2 class="col-md-12"> Style 2</h2>';
+							$alphas=range("A","Z");
+							$text = array();
+							foreach($alphas as $char){
+								$text[$char] = array();
+							}
+
 						    foreach ( $terms as $term ) {
 						        $first_letter = strtoupper(substr(trim($term->name) , 0, 1));
 						        array_push($text[$first_letter], $term);
 						    }
+
+						    foreach ($text as $key => $terms) {
+
+								if( !empty($terms) ){
+									echo '<div class="col-md-3"><ul class="none-style"><li>'.$key.'</li>';
+									foreach ($terms as $term) {
+										echo '<li>'.$term->name.'</li>';
+									}
+									echo '</ul></div>';
+								}
+							}
+							echo '</div>';
 						}
 
-						foreach ($text as $key => $terms) {
-							//var_dump($key);
-							if( !empty($terms) ){
-								echo '<div class="col-md-3"><ul class="none-style"><li>'.$key.'</li>';
-								foreach ($terms as $term) {
-									echo '<li>'.$term->name.'</li>';
-								}
-								echo '</ul></div>';
-							}
-						}
+
 					?>
 				</div>
 			</div>
