19782c668128bc5068a2746f3698711683484476
danng <danhoat@gmail.com>
2017-08-05 00:36:51 +0700

    update

--- inc/social/class_social.php
+++ inc/social/class_social.php
@@ -25,13 +25,29 @@ Class Box_Social{
 		$user_id = $this->social_id_exists($userdata['social_id']); // get user id of this social id
 
 		if( !$user_id){
+
 			if( email_exists($userdata['user_email'] ) ){
 				return  new WP_Error( 'exists_email', __( "Sorry, that email address is already used!", "boxtheme" ) );
 			}
 			$userdata['role'] = FREELANCER;
-			$userdata['user_pass'] = wp_generate_password(12);
+			$userdata['user_pass'] = wp_generate_password(15);
+
 			$user_id = wp_insert_user($userdata);
 			update_user_meta( $user_id, 'social_id', $userdata['social_id'] );
+			// insert profile
+			$args = array(
+				'post_title' 	=> $userdata['user_login'] ,
+				'post_type'  	=> PROFILE,
+				'post_author' 	=> $user_id,
+				'post_status' 	=> 'publish',
+				'meta_input'	=> array(
+					HOUR_RATE => 0,
+					RATING_SCORE => 0,
+					)
+			);
+			$profile_id = wp_insert_post($args);
+			update_user_meta( $user_id, 'profile_id', $profile_id );
+
 			global $wpdb;
 			$wpdb->update( $wpdb->users, array( 'user_status' => 1 ), array( 'user_login' => $userdata['user_login'] ) );
 			$wpdb->update( $wpdb->users, array( 'user_activation_key' => '' ), array( 'user_login' => $userdata['user_login'] ) );
