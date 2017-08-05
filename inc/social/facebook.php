8deec72fdf82ff4e4822576b9424d9391360a14b
danng <danhoat@gmail.com>
2017-08-05 09:25:48 +0700

    update

--- inc/social/facebook.php
+++ inc/social/facebook.php
@@ -11,75 +11,76 @@ class BX_Facebook{
 		add_action( 'wp_head', array($this, 'add_fb_script') );
 	}
 	public static function add_fb_script(){
-		global $box_option;
-		$social_api = $box_option->get_group_option('social_api');
-		$facebook = (object) $social_api['facebook'];	?>
-		<div id="fb-root"></div>
-		<script>
-			window.fbAsyncInit = function() {
-				FB.init({
-					//appId      : '256824294820471',
-					appId      : '<?php echo $facebook->app_id;?>',
-					cookie     : true,
-					xfbml      : true,
-					version    : 'v2.8'
-				});
-				//FB.AppEvents.logPageView();
-			};
+		if( is_page_template('page-login.php' ) || is_page_template('page-signup.php' ) ){
+			global $box_option;
+			$social_api = $box_option->get_group_option('social_api');
+			$facebook = (object) $social_api['facebook'];	?>
+			<div id="fb-root"></div>
+			<script>
+				window.fbAsyncInit = function() {
+					FB.init({
+						//appId      : '256824294820471',
+						appId      : '<?php echo $facebook->app_id;?>',
+						cookie     : true,
+						xfbml      : true,
+						version    : 'v2.8'
+					});
+					//FB.AppEvents.logPageView();
+				};
+				(function(d, s, id){
+				   	var js, fjs = d.getElementsByTagName(s)[0];
+				   	if (d.getElementById(id)) {return;}
+				   	js = d.createElement(s); js.id = id;
+				   	js.src = "//connect.facebook.net/en_US/sdk.js";
+				   	fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));
 
-			(function(d, s, id){
-			   	var js, fjs = d.getElementsByTagName(s)[0];
-			   	if (d.getElementById(id)) {return;}
-			   	js = d.createElement(s); js.id = id;
-			   	js.src = "//connect.facebook.net/en_US/sdk.js";
-			   	fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));
+				function shoModalLogin(){
+					FB.login( function(response) {
+						if (response.authResponse) {
+							FB.api('/me', {fields: 'name, email'}, function (response) {
+								var data = {user_login: response.name,type:'facebook', social_id: response.id, user_email: response.email };
+							   	jQuery.ajax({
+							        url : bx_global.ajax_url,
+							        emulateJSON: true,
+			       					method :'post',
 
-			function shoModalLogin(){
-				FB.login( function(response) {
-					if (response.authResponse) {
-						FB.api('/me', {fields: 'name, email'}, function (response) {
-							var data = {user_login: response.name,type:'facebook', social_id: response.id, user_email: response.email };
-						   	jQuery.ajax({
-						        url : bx_global.ajax_url,
-						        emulateJSON: true,
-		       					method :'post',
-
-								data: {
-									action: 'social_signup',
-									request: data,
-								},
-								beforeSend  : function(event){
-									console.log(data);
-						        	console.log('bat dau line 87');
-						        },
-						        success : function(res){
-						        	console.log(res);
-						        	if ( res.success){
-							        	if(res.redirect_url){
-							        		window.location.href = res.redirect_url;
-							        	} else {
-							        		window.location.href = bx_global.home_url;
-							        	}
-							        } else {
-							        	if(res.redirect_url){
-							        		window.location.href = res.redirect_url;
-							        	} else {
-							        		alert(res.msg);
-							        	}
+									data: {
+										action: 'social_signup',
+										request: data,
+									},
+									beforeSend  : function(event){
+										console.log(data);
+							        	console.log('bat dau line 87');
+							        },
+							        success : function(res){
+							        	console.log(res);
+							        	if ( res.success){
+								        	if(res.redirect_url){
+								        		window.location.href = res.redirect_url;
+								        	} else {
+								        		window.location.href = bx_global.home_url;
+								        	}
+								        } else {
+								        	if(res.redirect_url){
+								        		window.location.href = res.redirect_url;
+								        	} else {
+								        		alert(res.msg);
+								        	}
+								        }
 							        }
-						        }
-							});
-						}); //end FB.api
-					} // end success;
+								});
+							}); //end FB.api
+						} // end success;
 
-				}, { scope: 'email,public_profile' } );
-			}
-		</script>
-	<?php
+					}, { scope: 'email,public_profile' } );
+				}
+			</script>
+			<?php
+		}
 	}
-
 }
+
 function btn_fb_login(){ ?>
 	<li class="fb-item">
 		<a href="#" class="btn-facebook" onclick="shoModalLogin()">
