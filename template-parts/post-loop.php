9f32049b947539dd265baa77332410c6cd1596a3
danng <danhoat@gmail.com>
2017-08-06 23:55:23 +0700

    update

--- template-parts/post-loop.php
+++ template-parts/post-loop.php
@@ -1,10 +1,10 @@
 <div class="full">
-	<h3><a href="<?php the_permalink();?>"><?php the_title(); ?></a> </h3>
+	<h3 class="h3"><a href="<?php the_permalink();?>"><?php the_title(); ?></a> </h3>
 	<small><?php the_date(); ?></small>
 	<div class="post-excerpt">
 		<?php
 			if( has_post_thumbnail() ){
-				the_post_thumbnail('full');
+				the_post_thumbnail( 'full' );
 			}
 		?>
 		<?php the_excerpt(); ?>
