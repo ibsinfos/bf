4897a8f03ada90f6787040f17c29cf84cfecb154
danng <danhoat@gmail.com>
2017-08-07 00:11:05 +0700

    updatE

--- page.php
+++ page.php
@@ -12,8 +12,8 @@
 				<h1><?php the_title();?></h1>
 				<?php the_content(); ?>
 			</div>
-			<div class="col-md-4">
-				<?php get_sidebar('single');?>
+			<div class="col-md-4 sidebar" id="sidebar">
+				<?php get_sidebar('main_sidebar');?>
 			</div>
 		</div>
 	</div>
