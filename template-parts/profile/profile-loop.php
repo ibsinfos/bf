bd836164cd031dfabe696f53f1da374685f88045
danng <danhoat@gmail.com>
2017-08-25 17:18:04 +0700

    update

--- template-parts/profile/profile-loop.php
+++ template-parts/profile/profile-loop.php
@@ -23,16 +23,16 @@ if ((int) $profile->{RATING_SCORE} != $profile->{RATING_SCORE}){
 ?>
 <div class="row archive-profile-item">
 	<div class="full">
-		<div class="col-md-2 no-padding col-xs-12 col-avatar">
+		<div class="col-md-2 no-padding col-xs-4 col-avatar">
 		<?php echo '<a class="avatar" href = "'.get_author_posts_url($profile->post_author).'">'.get_avatar($profile->post_author, 150).'</a>';
 		$userdata = get_userdata($post->post_author); ?>
 		</div>
-		<div class="col-md-10 align-left  col-xs-12 res-content">
+		<div class="col-md-10 align-left  col-xs-8 res-content res-second-line">
 
-			<h3 class="profile-title no-margin col-xs-6">
+			<h3 class="profile-title no-margin col-xs-12">
 				<?php echo '<a class="" href = " '.get_author_posts_url($profile->post_author).'">'.$profile->post_title.'</a>';?>
 			</h3>
-			<span class="inline second-line col-md-12 col-xs-6">
+			<span class="inline second-line col-md-12 col-xs-12">
 				<span class="item professional-title primary-color">
 					<?php if( !empty($profile->professional_title) ){?>
 						<?php echo $profile->professional_title;?>
@@ -42,10 +42,12 @@ if ((int) $profile->{RATING_SCORE} != $profile->{RATING_SCORE}){
 
 			<span class="inline list-info ">
 				<span class=" item hour-rate "> <span class="glyphicon glyphicon-time"> </span> $<?php echo $profile->hour_rate;?>/hr </span>
-				<span class=" item eared-txt  "> Earned: <?php echo $profile->{EARNED};?> </span>
+				<span class=" item eared-txt   "> Earned: <?php echo $profile->{EARNED};?> </span>
 				<span class=" item country-profile "> <span class="glyphicon glyphicon-map-marker"></span>  <?php echo $profile->country;?> </span>
 				<span class="item profile-rating "> <start class="rating-score clear block <?php echo $start_class;?> "><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span></start></span>
 			</span>
+		</div>
+		<div class="col-md-10 align-left  col-xs-12 res-content">
 			<span class="overview-profile clear col-xs-12">
 			<?php
 			$str = str_replace( array("\n", "\r"), '', get_the_content() );
@@ -55,6 +57,7 @@ if ((int) $profile->{RATING_SCORE} != $profile->{RATING_SCORE}){
 			</span>
 			<small class="clear skills"><?php echo $skill_val;?></small>
 		</div>
+
 
 	</div>
 </div>
