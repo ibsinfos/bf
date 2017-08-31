7eca2f31a4e04077999a81857e3040d298733783
danng <danhoat@gmail.com>
2017-08-31 11:35:07 +0700

    update

--- header.php
+++ header.php
@@ -45,18 +45,16 @@ $role = bx_get_user_role();
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<?php
+	$html_logo = get_custom_logo();
+	$default_logo = '<img class="logo" src="'.get_theme_file_uri('img/logo.png').'" />';
+?>
 <?php do_action('before_header_menu' );?>
 <div class="row-nav full-width header" id="full_header">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-2 col-logo">
-				<a class="logo" href="<?php echo home_url();?>">
-					<?php
-						$html_logo = get_custom_logo();
-						$default_logo = '<img class="logo" src="'.get_theme_file_uri('img/logo.png').'" />';
-						echo !empty($html_logo) ? $html_logo : $default_logo;
-					?>
-				</a>
+				<a class="logo" href="<?php echo home_url();?>"> <?php echo !empty( $html_logo ) ? $html_logo : $default_logo; ?>	</a>
 			</div>
 			<div class="no-padding col-nav col-md-6">
 				<?php if ( has_nav_menu( 'top' ) ) { get_template_part( 'template-parts/navigation', 'top' );} ?>
@@ -64,7 +62,7 @@ $role = bx_get_user_role();
 			<!-- seach form here !-->
 			<div class="col-md-3 f-right align-right no-padding-left header-action">
 				<?php
-					if ( is_user_logged_in() ) { box_account_dropdow_menu();} else { ?>
+					if ( is_user_logged_in() ) { box_account_dropdow_menu(); } else { ?>
 					<ul class="main-login">
 						<li class="login text-center desktop-only ">
 							<a href="<?php echo bx_get_static_link('login');?>" class="sign-text btn btn-login"><?php _e('Log In','boxtheme');?></a>
