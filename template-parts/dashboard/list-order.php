e8f8c5270379cf7474d34f08545fb5949245b41c
danng <danhoat@gmail.com>
2017-09-02 17:41:29 +0700

    update

--- template-parts/dashboard/list-order.php
+++ template-parts/dashboard/list-order.php
@@ -2,15 +2,15 @@
 <div class="col-md-12">
 
   	<h5> <?php _e('History of credit','boxtheme');?> </h5>
-  	<ul class="none-style row">
-  		<li>
+  	<div class="none-style row history-order">
+  		<div class="full line-heading">
   			<div class="col-md-4">Date </div>
   			<div class="col-md-2">Type </div>
   			<div class="col-md-2">Payment </div>
   			<div class="col-md-2">Status </div>
   			<div class="col-md-2">Balance </div>
   			<!-- <div class="col-md-2"> </div> -->
-  		</li>
+  		</div>
   		<?php
   		// query order of this user.
   		$args = array(
@@ -33,15 +33,33 @@
   				if( in_array($order->order_type, array('withdraw','pay_service') ) )
   					$check = '(-)';
   				?>
-  				<div class="col-md-4"><?php echo $post->ID; echo get_the_date();?> </div>
-  				<div class="col-md-2"><?php echo $types[$order->order_type];?> </div>
-      			<div class="col-md-2"><?php echo $order->payment_type;?> </div>
-      			<div class="col-md-2"><?php echo $status[$order->post_status];?> </div>
-      			<div class="col-md-2"><?php echo $order->amout . $check;?>  </div>
+  				<div class="line full">
+	  				<div class="col-md-4"><?php echo get_the_date();?> </div>
+	  				<div class="col-md-2"><?php echo $types[$order->order_type];?> </div>
+	      			<div class="col-md-2"><?php echo $order->payment_type;?> </div>
+	      			<div class="col-md-2"><?php echo $status[$order->post_status];?> </div>
+	      			<div class="col-md-2"><?php echo $order->amout . $check;?>  </div>
+      			</div>
       			<!-- <div class="col-md-2"><button>Arhive</button> </div> -->
   				<?php
   			}
   		}
   		?>
-  	</ul>
+  	</div>
 </div>
+<style type="text/css">
+	.history-order{
+		border:1px solid #e6e6e6;
+	}
+	.history-order .line.full{
+		padding: 8px 0;
+		border-bottom: 1px solid #e6e6e6;
+		overflow: hidden;
+	}
+	div.full.line-heading{
+		border-bottom: 1px solid #e6e6e6;
+		overflow: hidden;
+		padding: 10px 0;
+		font-weight: bold;
+	}
+</style>
