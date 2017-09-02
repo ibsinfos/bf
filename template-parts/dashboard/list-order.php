c846e43822c944b2730976e7ee1ee4d4e440ef36
danng <danhoat@gmail.com>
2017-09-02 17:34:11 +0700

    update

--- template-parts/dashboard/list-order.php
+++ template-parts/dashboard/list-order.php
@@ -4,12 +4,12 @@
   	<h5> <?php _e('History of credit','boxtheme');?> </h5>
   	<ul class="none-style row">
   		<li>
-  			<div class="col-md-2">Date </div>
+  			<div class="col-md-4">Date </div>
   			<div class="col-md-2">Type </div>
   			<div class="col-md-2">Payment </div>
   			<div class="col-md-2">Status </div>
   			<div class="col-md-2">Balance </div>
-  			<div class="col-md-2">Actions </div>
+  			<!-- <div class="col-md-2"> </div> -->
   		</li>
   		<?php
   		// query order of this user.
@@ -21,6 +21,7 @@
   			'posts_per_page' => -1,
   		);
   		$status = array('pending' => __('Pending','boxtheme'),'publish' => __('Approved','boxtheme') );
+		$types = array('buy_credit' => __('Buy credit','boxtheme'),'withdraw' => __('Withdraw','boxtheme') ,'none' =>'None');
   		$query = new WP_Query($args);
   		if( $query->have_posts() ){
   			while ( $query->have_posts() ) {
@@ -32,12 +33,12 @@
   				if( in_array($order->order_type, array('withdraw','pay_service') ) )
   					$check = '(-)';
   				?>
-  				<div class="col-md-2"><?php echo get_the_date();?> </div>
-  				<div class="col-md-2"><?php echo $order->order_type;?> </div>
+  				<div class="col-md-4"><?php echo $post->ID; echo get_the_date();?> </div>
+  				<div class="col-md-2"><?php echo $types[$order->order_type];?> </div>
       			<div class="col-md-2"><?php echo $order->payment_type;?> </div>
       			<div class="col-md-2"><?php echo $status[$order->post_status];?> </div>
       			<div class="col-md-2"><?php echo $order->amout . $check;?>  </div>
-      			<div class="col-md-2"><button>Arhive</button> </div>
+      			<!-- <div class="col-md-2"><button>Arhive</button> </div> -->
   				<?php
   			}
   		}
