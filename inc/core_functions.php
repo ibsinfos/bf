33d71a1ecfc773e163922d7e699a45917ba42bee
danng <danhoat@gmail.com>
2017-09-04 19:48:46 +0700

    udpate

--- inc/core_functions.php
+++ inc/core_functions.php
@@ -398,3 +398,24 @@ function list_currency(){
 		'ZMW' => 'Zambian kwacha (ZK)',
 	);
 }
+add_filter('getimagesize_mimes_to_exts','bx_allow_upload_extend');
+function bx_allow_upload_extend($args){
+
+	$add = array('doc|docx' => 'application/msword' ,
+		'pdf' 			=> 'application/pdf',
+		'zip' 			=> 'multipart/x-zip',
+		'mp3|m4a|m4b'  => 'audio/mpeg',
+		'wav'          => 'audio/wav',
+		'wma'          => 'audio/x-ms-wma',
+		'wmv'          => 'video/x-ms-wmv',
+		'wmx'          => 'video/x-ms-wmx',
+		'wm'           => 'video/x-ms-wm',
+		'avi'          => 'video/avi',
+		'divx'         => 'video/divx',
+		'flv'          => 'video/x-flv',
+		'mov|qt'       => 'video/quicktime',
+		'mpeg|mpg|mpe' => 'video/mpeg',
+		'mp4|m4v'      => 'video/mp4'
+		);
+	return array_merge($args, $add);
+}
--- inc/requires.php
+++ inc/requires.php
@@ -3,7 +3,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 require get_parent_theme_file_path( '/inc/define.php' );
 require get_parent_theme_file_path( '/inc/class-bx-install.php' );
 
-require get_parent_theme_file_path( '/inc/wp_core.php' );
+require get_parent_theme_file_path( '/inc/core_wp.php' );
 require get_parent_theme_file_path( '/inc/core_functions.php' );
 require get_parent_theme_file_path( '/inc/plugable.php' );
 require get_parent_theme_file_path( '/inc/themes.php' );
@@ -48,25 +48,5 @@ require get_parent_theme_file_path( '/inc/class_customizer.php' );
  */
 ///require get_parent_theme_file_path( '/inc/customizer.php' );
 
-add_filter('getimagesize_mimes_to_exts','bx_allow_upload_extend');
-function bx_allow_upload_extend($args){
 
-	$add = array('doc|docx' => 'application/msword' ,
-		'pdf' 			=> 'application/pdf',
-		'zip' 			=> 'multipart/x-zip',
-		'mp3|m4a|m4b'  => 'audio/mpeg',
-		'wav'          => 'audio/wav',
-		'wma'          => 'audio/x-ms-wma',
-		'wmv'          => 'video/x-ms-wmv',
-		'wmx'          => 'video/x-ms-wmx',
-		'wm'           => 'video/x-ms-wm',
-		'avi'          => 'video/avi',
-		'divx'         => 'video/divx',
-		'flv'          => 'video/x-flv',
-		'mov|qt'       => 'video/quicktime',
-		'mpeg|mpg|mpe' => 'video/mpeg',
-		'mp4|m4v'      => 'video/mp4'
-		);
-	return array_merge($args, $add);
-}
 ?>
