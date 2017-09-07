9d63e735070bb848d2c154f868aabe68be325c05
danng <danhoat@gmail.com>
2017-09-08 03:30:47 +0700

    udpate text

--- static-block/one-how-we-work.php
+++ static-block/one-how-we-work.php
@@ -16,21 +16,21 @@
 				<div class="full center">
 					<div class="img_main"><img src="<?php echo get_stylesheet_directory_uri();?>/img/bid-a-job.png"></div>
 					<div class="full center"><h3>BID A JOB</h3></div>
-					<div class="full">Post a job to tell us about your project. We'll quickly match you with the right freelancers.</div>
+					<div class="full">Find a right project then bidding on that project then wait the feedback if have</div>
 				</div>
 			</div>
 			<div class="col-md-3 ">
 				<div class="full center">
 					<div class="img_main"><img src="<?php echo get_stylesheet_directory_uri();?>/img/done-work.png"></div>
 					<div class="full center"><h3> DONE WORK</h3></div>
-					Post a job to tell us about your project. We'll quickly match you with the right freelancers.
+					Affter Employer assign and deposit for a job. Freelancer can work on this task.
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="full center">
 					<div class="img_main"><img src="<?php echo get_stylesheet_directory_uri();?>/img/earn.png"></div>
 					<div class="full center"><h3> GET PAID</h3></div>
-					Post a job to tell us about your project. We'll quickly match you with the right freelancers.
+					Finish the project, submit the result to employer then get paid from the employer.
 				</div>
 			</div>
 
