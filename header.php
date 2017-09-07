ff7e795fe7f239d34cc03d98501c8e5097b84bcc
danng <danhoat@gmail.com>
2017-09-08 03:06:12 +0700

    update

--- header.php
+++ header.php
@@ -52,32 +52,30 @@ $role = bx_get_user_role();
 <?php do_action('before_header_menu' );?>
 <div class="row-nav full-width header" id="full_header">
 	<div class="container">
-		<div class="row">
-			<div class="col-md-2 col-logo">
-				<a class="logo" href="<?php echo home_url();?>"> <?php echo !empty( $html_logo ) ? $html_logo : $default_logo; ?>	</a>
-			</div>
-			<div class="no-padding col-nav col-md-6">
-				<?php if ( has_nav_menu( 'top' ) ) { get_template_part( 'template-parts/navigation', 'top' );} ?>
-			</div>
-			<!-- seach form here !-->
-			<div class="col-md-3 f-right align-right no-padding-left header-action">
-				<?php
-					if ( is_user_logged_in() ) { box_account_dropdow_menu(); } else { ?>
-					<ul class="main-login">
-						<li class="login text-center desktop-only ">
-							<a href="<?php echo bx_get_static_link('login');?>" class="sign-text btn btn-login"><?php _e('Log In','boxtheme');?></a>
-						</li>
-						<li class=" sign-up desktop-only">
-							<a href="<?php echo bx_get_static_link('signup');?>" class="btn btn-signup sign-text"> <?php _e('Sign Up','boxtheme');?></a>
-						</li>
-						<li class=" mobile-only">
-							<button type="button" class="btn btn-login " data-toggle="modal" data-target="#loginModal">
-		  						<span class="glyphicon glyphicon-user login-icon"></span>
-							</button>
-						</li>
-					</ul>
-				<?php } ?>
-			</div> <!-- .header-action !-->
+		<div class="col-md-2 col-logo">
+			<a class="logo" href="<?php echo home_url();?>"> <?php echo !empty( $html_logo ) ? $html_logo : $default_logo; ?>	</a>
 		</div>
+		<div class="no-padding col-nav col-md-6">
+			<?php if ( has_nav_menu( 'top' ) ) { get_template_part( 'template-parts/navigation', 'top' );} ?>
+		</div>
+		<!-- seach form here !-->
+		<div class="col-md-3 f-right align-right no-padding-left header-action">
+			<?php
+				if ( is_user_logged_in() ) { box_account_dropdow_menu(); } else { ?>
+				<ul class="main-login">
+					<li class="login text-center desktop-only ">
+						<a href="<?php echo bx_get_static_link('login');?>" class="sign-text btn btn-login"><?php _e('Log In','boxtheme');?></a>
+					</li>
+					<li class=" sign-up desktop-only">
+						<a href="<?php echo bx_get_static_link('signup');?>" class="btn btn-signup sign-text"> <?php _e('Sign Up','boxtheme');?></a>
+					</li>
+					<li class=" mobile-only">
+						<button type="button" class="btn btn-login " data-toggle="modal" data-target="#loginModal">
+	  						<span class="glyphicon glyphicon-user login-icon"></span>
+						</button>
+					</li>
+				</ul>
+			<?php } ?>
+		</div> <!-- .header-action !-->
 	</div>	<!-- .navigation-top -->
 </div>
