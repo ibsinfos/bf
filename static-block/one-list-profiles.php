57afaa9a192bea790d31d786c989eac8345d4a62
danng <danhoat@gmail.com>
2017-09-12 08:36:14 +0700

    update

--- static-block/one-list-profiles.php
+++ static-block/one-list-profiles.php
@@ -1,85 +1,86 @@
-<section class="full-width top-profile">
-	<div class=" container site-container">
-		<div class="row">
-			<div class="col-md-12">
-				<h2 class="pypl-heading"> Looking for Professional Freelancers?</h2>
-			</div>
-			<?php
-			$profile_query = new WP_Query( array (
-				'post_type' => PROFILE,
-				'post_status' => 'publish',
-				'orderby'   => 'meta_value_num',
-				'meta_key'  => RATING_SCORE,
-				'order'     => 'DESC',
-				'orderby'    => 'meta_value_num',
-				'order'      => 'DESC',
-				'showposts' => 6
-				)
-			);
-			if( $profile_query->have_posts() ){
-				while( $profile_query->have_posts() ){
-					global $post;
-					$profile_query->the_post();
-					$profile 	= BX_Profile::get_instance()->convert($post);
-					$profile_id = get_user_meta($post->post_author,'profile_id', true);
-					$rating = get_post_meta($post->ID,RATING_SCORE, true);
-					$start_class = 'core-'.$profile->{RATING_SCORE};
-					if ((int) $profile->{RATING_SCORE} != $profile->{RATING_SCORE}){
-						$start_class = 'score-'.(int)$profile->{RATING_SCORE}.'-half';
-					}
+<?php
+$profile_query = new WP_Query( array (
+	'post_type' => PROFILE,
+	'post_status' => 'publish',
+	'orderby'   => 'meta_value_num',
+	'meta_key'  => RATING_SCORE,
+	'order'     => 'DESC',
+	'orderby'    => 'meta_value_num',
+	'order'      => 'DESC',
+	'showposts' => 6
+	)
+);
+if( $profile_query->have_posts() ){ ?>
+	<section class="full-width top-profile">
+		<div class=" container site-container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="pypl-heading"> Looking for Professional Freelancers?</h2>
+				</div> <?php
 
-					$skills = get_the_terms( $profile_id, 'skill' );
-					$skill_html = '';
+					while( $profile_query->have_posts() ){
+						global $post;
+						$profile_query->the_post();
+						$profile 	= BX_Profile::get_instance()->convert($post);
+						$profile_id = get_user_meta($post->post_author,'profile_id', true);
+						$rating = get_post_meta($post->ID,RATING_SCORE, true);
+						$start_class = 'core-'.$profile->{RATING_SCORE};
+						if ((int) $profile->{RATING_SCORE} != $profile->{RATING_SCORE}){
+							$start_class = 'score-'.(int)$profile->{RATING_SCORE}.'-half';
+						}
 
-					if ( $skills && ! is_wp_error( $skills ) ){
-					  	$draught_links = array();
-					  	foreach ( $skills as $term ) {
-					     	//$draught_links[] = '<a href="'.get_term_link($term).'">'.$term->name.'</a>';
-					     	$draught_links[] = '<span >'.$term->name.'</span>';
-					     	$list_ids[] = $term->term_id;
-					  	}
-					  	$skill_html = join( ", ", $draught_links );
-					}?>
+						$skills = get_the_terms( $profile_id, 'skill' );
+						$skill_html = '';
 
-					<div class="col-md-6 col-xs-12 profile-item" >
-						<div class="full box-bg">
-							<div class="left avatar col-md-4 col-xs-5 no-padding-right">
-								<?php echo '<a class="primary-color" href = "'.get_author_posts_url($profile->post_author).'">'.get_avatar($profile->post_author).'</a>';
-								$userdata = get_userdata($post->post_author); ?>
-							</div>
-							<div class="right col-md-8 col-xs-7">
-								<h3 class="profile-title no-margin">
-									<?php echo '<a class="" href = " '.get_author_posts_url($profile->post_author).'">'.$profile->post_title.'</a>';?>
-								</h3>
-								<h5 class="professional-title primary-color">
-									<?php if( !empty($profile->professional_title) ){ ?>
-										<?php echo $profile->professional_title;?>
-									<?php } ?>
-								</h5>
-								<span class="absolute abs-top abs-right-15 hour-rate ">$<?php echo $profile->hour_rate;?>/hr</span>
-								<span class="padding-top-15"><span>Join since June, 2017 </span></span>
-								<span class="full">
-									<start class="rating-score <?php echo $start_class;?> ">
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-									</start>
-									<span></span>
-									<span class="absolute  abs-right-15"><img src="<?php echo get_stylesheet_directory_uri();?>/img/flag.png"></span>
-								</span>
-							</div>
-							<div class="right col-md-12 list-skill padding-top-10 ">
-									Skills: <?php echo $skill_html;?>
+						if ( $skills && ! is_wp_error( $skills ) ){
+						  	$draught_links = array();
+						  	foreach ( $skills as $term ) {
+						     	//$draught_links[] = '<a href="'.get_term_link($term).'">'.$term->name.'</a>';
+						     	$draught_links[] = '<span >'.$term->name.'</span>';
+						     	$list_ids[] = $term->term_id;
+						  	}
+						  	$skill_html = join( ", ", $draught_links );
+						}?>
+
+						<div class="col-md-6 col-xs-12 profile-item" >
+							<div class="full box-bg">
+								<div class="left avatar col-md-4 col-xs-5 no-padding-right">
+									<?php echo '<a class="primary-color" href = "'.get_author_posts_url($profile->post_author).'">'.get_avatar($profile->post_author).'</a>';
+									$userdata = get_userdata($post->post_author); ?>
 								</div>
-						</div>
-					</div> <?php
+								<div class="right col-md-8 col-xs-7">
+									<h3 class="profile-title no-margin">
+										<?php echo '<a class="" href = " '.get_author_posts_url($profile->post_author).'">'.$profile->post_title.'</a>';?>
+									</h3>
+									<h5 class="professional-title primary-color">
+										<?php if( !empty($profile->professional_title) ){ ?>
+											<?php echo $profile->professional_title;?>
+										<?php } ?>
+									</h5>
+									<span class="absolute abs-top abs-right-15 hour-rate ">$<?php echo $profile->hour_rate;?>/hr</span>
+									<span class="padding-top-15"><span>Join since June, 2017 </span></span>
+									<span class="full">
+										<start class="rating-score <?php echo $start_class;?> ">
+											<i class="fa fa-star" aria-hidden="true"></i>
+											<i class="fa fa-star" aria-hidden="true"></i>
+											<i class="fa fa-star" aria-hidden="true"></i>
+											<i class="fa fa-star" aria-hidden="true"></i>
+											<i class="fa fa-star" aria-hidden="true"></i>
+										</start>
+										<span></span>
+										<span class="absolute  abs-right-15"><img src="<?php echo get_stylesheet_directory_uri();?>/img/flag.png"></span>
+									</span>
+								</div>
+								<div class="right col-md-12 list-skill padding-top-10 ">
+										Skills: <?php echo $skill_html;?>
+									</div>
+							</div>
+						</div> <?php
 
-				}?>
-				<div class="col-md-12 f-right align-right" ><a href="<?php echo get_post_type_archive_link('profile');?>" class="primary-color view-all"> <?php _e('View All Profiles','boxtheme');?> &nbsp; <span class=" glyphicon glyphicon-menu-right"></span></a></div>
-			<?php }?>
+					}?>
+					<div class="col-md-12 f-right align-right" ><a href="<?php echo get_post_type_archive_link('profile');?>" class="primary-color view-all"> <?php _e('View All Profiles','boxtheme');?> &nbsp; <span class=" glyphicon glyphicon-menu-right"></span></a></div>
 
-		</div>
-	</div> <!-- .row !-->
-</section>
+			</div>
+		</div> <!-- .row !-->
+	</section>
+<?php } ?>
--- static-block/one-package-plan.php
+++ static-block/one-package-plan.php
@@ -1,30 +1,34 @@
-<section class="full-width packge-plan">
-	<div class="container">
-		<div class="row"><?php
-			$buy_link = box_get_static_link('buy-credit');
-			wp_reset_query();
-			$args = array(
-				'post_type' => '_package',
-				'posts_per_page' =>3
-			);
-			$result = new WP_Query($args);
-			if( $result->have_posts() ){
-				while( $result->have_posts() ){
-					$result->the_post();
-					$price = get_post_meta(get_the_ID(),'price', true); ?>
-					<div class="col-md-4 package-item">
-						<div class="pricing-table-plan">
-							<header data-plan="basic" class="pricing-plan-header basic-plan"><span class="plan-name"><?php the_title();?></span></header>
-				    		<div class="plan-features">
-					    		<span class="plan-monthly primary-color"><?php box_price($price);?></span>
-					    		<span class="pack-des">	<?php the_content();?> </span>
+<?php
+wp_reset_query();
+$buy_link = box_get_static_link('buy-credit');
+
+$args = array(
+	'post_type' => '_package',
+	'posts_per_page' =>3
+);
+$result = new WP_Query($args);
+if( $result->have_posts() ){ ?>
+	<section class="full-width packge-plan">
+		<div class="container">
+			<div class="row"><?php
+
+					while( $result->have_posts() ){
+						$result->the_post();
+						$price = get_post_meta(get_the_ID(),'price', true); ?>
+						<div class="col-md-4 package-item">
+							<div class="pricing-table-plan">
+								<header data-plan="basic" class="pricing-plan-header basic-plan"><span class="plan-name"><?php the_title();?></span></header>
+					    		<div class="plan-features">
+						    		<span class="plan-monthly primary-color"><?php box_price($price);?></span>
+						    		<span class="pack-des">	<?php the_content();?> </span>
+								</div>
+								<?php $link = add_query_arg( array('id' =>get_the_ID() ), $buy_link ); ?>
+					            <a class="btn btn-primary btn-xlarge " href="<?php echo esc_url($link);?>">Check out</a>
 							</div>
-							<?php $link = add_query_arg( array('id' =>get_the_ID() ), $buy_link ); ?>
-				            <a class="btn btn-primary btn-xlarge " href="<?php echo esc_url($link);?>">Check out</a>
 						</div>
-					</div>
-					<?php } ;?>
-			<?php } ?>
-		</div> <!-- end row !-->
-	</div>
-</section>
+						<?php } ;?>
+
+			</div> <!-- end row !-->
+		</div>
+	</section>
+<?php } ?>
