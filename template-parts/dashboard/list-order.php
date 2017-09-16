92807b6261e9186025d14943d2ac67728d33dc54
danngyw <danng@youngworld.vn>
2017-09-17 01:13:46 +0700

    update

--- template-parts/dashboard/list-order.php
+++ template-parts/dashboard/list-order.php
@@ -33,11 +33,11 @@
   					$check = '(-)';
   				?>
   				<div class="line full">
-	  				<div class="col-md-4"><?php echo get_the_date();?> </div>
-	  				<div class="col-md-2 hidden-xs"><?php echo $types[$order->order_type];?> </div>
-	      			<div class="col-md-2 hidden-xs"><?php echo $order->payment_type;?> </div>
+	  				<div class="col-md-4 col-xs-5"><?php echo get_the_date();?> </div>
+	  				<div class="col-md-2 col-xs-3hidden-xs"><?php echo $types[$order->order_type];?> </div>
+	      			<div class="col-md-2 "><?php echo $order->payment_type;?> </div>
 	      			<div class="col-md-2 hidden-xs"><?php echo $status[$order->post_status];?> </div>
-	      			<div class="col-md-2 align-center"><?php echo $order->amout . $check;?>  </div>
+	      			<div class="col-md-2 align-center col-xs-4"><?php echo $order->amout . $check;?>  </div>
       			</div>
       			<!-- <div class="col-md-2"><button>Arhive</button> </div> -->
   				<?php
