e493c18c812ae1ecfb2b1685713f7fc3e5ccb613
danngyw <danng@youngworld.vn>
2017-10-08 10:58:44 +0700

    update

--- template-parts/dispute.php
+++ template-parts/dispute.php
@@ -2,6 +2,8 @@
 	<div class="row">
 		<div class="col-md-12">
 			<h3> This is dispute section </h3>
+			<p>Abc mark as disputed this job in 10/10/2016 and with the reason:</p>
+			<i> I'm not happy with the result - It doesn't match with my requirement </i>
 		</div>
 	</div>
 </div>
