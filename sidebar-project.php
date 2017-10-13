88d11db8494b885111bd2bee7b3eab88738f873c
danngyw <danng@youngworld.vn>
2017-10-14 01:43:38 +0700

    update

--- sidebar-project.php
+++ sidebar-project.php
@@ -1,4 +1,13 @@
 <?php
+function show_bid_buton($post){
+	$back_url = add_query_arg( 'redirect', get_the_permalink($post->ID), box_get_static_link('login') );
+	if( is_user_logged_in() ){
+		echo '<a class ="btn-bid-now text-right pull-right" href ="'.$back_url.'"> &nbsp; '.__('Bid Now','boxtheme').' &nbsp; &nbsp;  <i class="fa fa-angle-right" aria-hidden="true"></i> </a>';
+	} else {
+		echo '<a class ="btn-login text-right pull-right" href ="'.$back_url.'"> &nbsp; '.__('Login to bid','boxtheme').' &nbsp; &nbsp;  <i class="fa fa-angle-right" aria-hidden="true"></i> </a>';
+	}
+}
+
 global $user_ID, $project, $is_owner, $access_workspace, $is_workspace, $winner_id, $class_bidded, $bidding, $is_logged, $current_user_can_bid;
 $user = get_userdata($project->post_author );
 
@@ -16,6 +25,9 @@ $score = (int) get_user_meta($project->post_author,RATING_SCORE, true);
 if(empty($score) || !$score){
 	$score = 0;
 }
+
+
+
 ?>
 <div class="block-employer-info">
 	<h3> <?php _e('Employer Information','boxtheme');?></h3>
@@ -31,38 +43,31 @@ if(empty($score) || !$score){
 
 		</li>
 		<li><i class="fa fa-map-marker bcon" aria-hidden="true"></i><?php echo $txt_country;?></li>
-		<li><i class="fa fa-flag bcon" aria-hidden="true"></i><?php printf(__("Project posted: <span class='text-right pull-right'>%d</span>",'boxtheme'), $project_posted);?></li>
-		<li><i class="fa fa-address-book-o bcon" aria-hidden="true"></i><?php printf(__("Freelancers Hired: <span class='text-right pull-right'>%d</span>",'boxtheme'), $fre_hired);?></li>
-		<li><i class="fa fa-money bcon" aria-hidden="true"></i><?php printf(__("Total Spent ($): <span class='text-right pull-right'>%f</span>",'boxtheme'), $total_spent );?></li>
+		<li><i class="fa fa-flag bcon" aria-hidden="true"></i><?php printf(__("Project posted <span class='text-right pull-right'>%d</span>",'boxtheme'), $project_posted);?></li>
+		<li><i class="fa fa-address-book-o bcon" aria-hidden="true"></i><?php printf(__("Freelancers Hired <span class='text-right pull-right'>%d</span>",'boxtheme'), $fre_hired);?></li>
+		<li><i class="fa fa-money bcon" aria-hidden="true"></i><?php printf(__("Total Spent ($) <span class='text-right pull-right'>%f</span>",'boxtheme'), $total_spent );?></li>
 		<li class="rating rating-score score-<?php echo $score;?>"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></li>
 	</ul>
+</div>
 
-</div>
-<div class="company-pictures">
-		<img src="<?php echo  get_template_directory_uri().'/img/banner.jpg';?>" />
-</div>
 <?php
 
-function show_bid_buton($post){
-	$back_url = add_query_arg( 'redirect', get_the_permalink($post->ID), box_get_static_link('login') );
-	echo '<a class ="btn-login text-right pull-right" href ="'.$back_url.'"> &nbsp; '.__('Login to bid','boxtheme').' &nbsp; &nbsp;  <i class="fa fa-angle-right" aria-hidden="true"></i> </a>';
-}
+
 if ( $is_logged ) {
 
 	if ( $current_user_can_bid  ) { // chec post_status = publish and freelancer role.
 		// is freelancer and logged
 		if ( ! $bidding){
-			get_template_part( 'template-parts/project/bid', 'form' ); //bid_form include bid-form.php file
+			show_bid_buton($project);
+			//get_template_part( 'template-parts/project/bid', 'form' ); //bid_form include bid-form.php file
 		}
 
-	} else {
-		// emp, fre
-		$role = bx_get_user_role( $user_ID );
-		if ( is_owner_project( $project ) ) {
-		}
 	}
 } else if ( $project->post_status == 'publish' ) {
 	//is visitor
-	//show_bid_buton($project);
+	show_bid_buton($project);
 }
 ?>
+<div class="company-pictures">
+		<img src="<?php echo  get_template_directory_uri().'/img/banner.jpg';?>" />
+</div>
