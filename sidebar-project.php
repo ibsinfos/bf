305ae651397f5ce56a4b1677fe31e6cdf926b5be
danngyw <danng@youngworld.vn>
2017-10-14 01:08:11 +0700

    update

--- sidebar-project.php
+++ sidebar-project.php
@@ -11,7 +11,7 @@ if( !is_wp_error($ucountry ) ){
 }
 $project_posted = (int) get_user_meta( $project->post_author, 'project_posted', true);
 $fre_hired = (int) get_user_meta( $project->post_author, 'fre_hired', true);
-$total_spent =  get_user_meta( $project->post_author, 'total_spent', true);
+$total_spent = intval( get_user_meta( $project->post_author, 'total_spent', true) );
 $score = (int) get_user_meta($project->post_author,RATING_SCORE, true);
 if(empty($score) || !$score){
 	$score = 0;
@@ -33,7 +33,7 @@ if(empty($score) || !$score){
 		<li><i class="fa fa-map-marker bcon" aria-hidden="true"></i><?php echo $txt_country;?></li>
 		<li><i class="fa fa-flag bcon" aria-hidden="true"></i><?php printf(__("Project posted: <span class='text-right pull-right'>%d</span>",'boxtheme'), $project_posted);?></li>
 		<li><i class="fa fa-address-book-o bcon" aria-hidden="true"></i><?php printf(__("Freelancers Hired: <span class='text-right pull-right'>%d</span>",'boxtheme'), $fre_hired);?></li>
-		<li><i class="fa fa-money bcon" aria-hidden="true"></i><?php printf(__("Total Spent ($): <span class='text-right pull-right'>%s</span>",'boxtheme'), $total_spent );?></li>
+		<li><i class="fa fa-money bcon" aria-hidden="true"></i><?php printf(__("Total Spent ($): <span class='text-right pull-right'>%f</span>",'boxtheme'), $total_spent );?></li>
 		<li class="rating rating-score score-<?php echo $score;?>"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></li>
 	</ul>
 
