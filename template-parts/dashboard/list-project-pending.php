2e249b125710c0aa1e2ebbcbcfa72adc92ef2732
danngyw <danng@youngworld.vn>
2017-10-14 17:26:03 +0700

    update

---
+++ template-parts/dashboard/list-project-pending.php
@@ -0,0 +1,48 @@
+<?php
+	global $user_ID;
+	$args = array(
+		'post_status' => 'pending',
+		'post_type' => 'project',
+		'author'=> $user_ID,
+		'posts_per_page' => -1,
+	);
+	$query = new WP_Query($args);
+	$loadmore = false;
+	if( $query-> have_posts() ){
+		$loadmore = true;
+		echo '<ul class="db-list-project">';
+		echo '<li class="heading row list-style-none padding-bottom-10">';
+				echo '<div class ="col-md-6">';
+				_e('Project Title','boxtheme');
+				echo '</div>';
+				echo '<div class ="col-md-2">';
+				_e('Bid(s)','boxthenme');
+				echo '</div>';
+				echo '<div class ="col-md-2">';
+				_e('Price','boxtheme');
+				echo '</div>';
+				echo '<div class ="col-md-2">';
+				_e('Posted date','boxtheme');
+				echo '</div>';
+			echo '</li>';
+
+		while ($query->have_posts()) {
+			global $post;
+			$query->the_post();
+			$project = BX_Project::get_instance()->convert($post);
+			echo '<li class=" list-style-none padding-bottom-10">';
+				echo '<div class ="col-md-6">';
+				echo '<a href="'.get_permalink().'">'. get_the_title().'</a>';
+				echo '</div>';
+				echo '<div class ="col-md-2">';
+				echo count_bids($post->ID);
+				echo '</div>';
+				echo '<div class ="col-md-2">';
+				box_price($project->_budget);
+				echo '</div>';
+				echo '<div class ="col-md-2">';	echo get_the_date();	echo '</div>';
+			echo '</li>';
+		}
+		echo '</ul>';
+	}
+?>
