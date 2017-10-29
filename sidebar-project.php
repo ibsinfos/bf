bfd7623518131e3eceda4f1cc9a01350b055a4b1
danngyw <danng@youngworld.vn>
2017-10-29 20:30:31 +0700

    update

--- sidebar-project.php
+++ sidebar-project.php
@@ -24,12 +24,12 @@ if( !is_wp_error($ucountry ) ){
 }
 $project_posted = (int) get_user_meta( $project->post_author, 'project_posted', true);
 $fre_hired = (int) get_user_meta( $project->post_author, 'fre_hired', true);
-$total_spent = floatval( get_user_meta( $project->post_author, 'total_spent', true) );
+$total_spent = (float) get_user_meta( $project->post_author, 'total_spent', true);
 $score = (int) get_user_meta($project->post_author,RATING_SCORE, true);
 if(empty($score) || !$score){
 	$score = 0;
 }
-
+$employer = get_userdata($project->post_author);
 ?>
 	<div class="main-btn-react  hide">
 		<button class="contact-me primary-bg"> WorkSpace</button><button class="contact-me primary-bg">Dispute</button>
@@ -44,13 +44,13 @@ if(empty($score) || !$score){
 			</div>
 			<div class="right-emp-avatar">
 				<label class="emp-name"><a class="author-url" href="#<?php //echo esc_url(get_author_posts_url( $project->post_author )); ?>"><?php echo $user->display_name ;?>  </a> </label>
-				<span class="member-since"> Member Since May 16, 2017</span>
+				<span class="member-since"><?php printf( __("Member Since %s",'boxtheme'), date( "M d, Y", strtotime($employer->user_registered) ) );?></span>
 			</div>
 		</li>
 		<li><i class="fa fa-map-marker bcon" aria-hidden="true"></i>From <span class="pull-right"><?php echo $txt_country;?></span></li>
 		<li><i class="fa fa-flag bcon" aria-hidden="true"></i><?php printf(__("Project posted <span class='text-right pull-right'>%d</span>",'boxtheme'), $project_posted);?></li>
 		<li><i class="fa fa-address-book-o bcon" aria-hidden="true"></i><?php printf(__("Freelancers hired <span class='text-right pull-right'>%d</span>",'boxtheme'), $fre_hired);?></li>
-		<li><i class="fa fa-money bcon" aria-hidden="true"></i><?php printf(__("Total spent ($) <span class='text-right pull-right'>%f</span>",'boxtheme'), $total_spent );?></li>
+		<li><i class="fa fa-money bcon" aria-hidden="true"></i><?php printf( __( "Total spent ($) <span class='text-right pull-right'>%s</span>",'boxtheme'),floatval( $total_spent )  );?></li>
 		<li class="rating rating-score score-<?php echo $score;?>"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></li>
 		<?php if ( $current_user_can_bid && ! $bidding ) { ?>
 			<li><?php box_bid_buton($project);?></li>
