26d7515f8307f6456fb2fb44d9d4ba269aa50aa6
danngyw <danng@youngworld.vn>
2017-11-06 11:44:17 +0700

    update

--- inc/class_option.php
+++ inc/class_option.php
@@ -147,7 +147,7 @@ class BX_Option {
 			'new_account_noti' => array(
 				'receiver' => 'register',
 				'subject' =>	'Has new register in #blog_name site',
-				'content' =>	'<p>Hello administrator,</p> <p>This email to let you know that has a new register in #blog_name.</p><p>Here are the detail: </p><p> Username: <strong>#user_login</strong>.<br />Email: <strong>#user_email</strong>.</p>Sincerely,'
+				'content' =>	'<p>Hello administrator,</p> <p>This email to let you know that has a new register in <i> #blog_name </i> site.</p><p>Here are the detail: </p><p> Username: <strong>#user_login</strong>.<br />Email: <strong>#user_email</strong>.</p>Sincerely,'
 			),
 			'reset_password' => array(
 				'content' =>  '<p>Hi #display_name,</p><p><a href="#home_url">#blog_name</a> has received a request to reset the password for your account. If you did not request to reset your password, please ignore this email.</p>
