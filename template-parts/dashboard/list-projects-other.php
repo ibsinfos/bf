2073931e39945414c3ecdbb2f6e6732eaf5768af
danngyw <danng@youngworld.vn>
2017-11-09 00:04:54 +0700

    udpate

--- template-parts/dashboard/list-projects-other.php
+++ template-parts/dashboard/list-projects-other.php
@@ -43,11 +43,11 @@
 			$query = new WP_Query($args);
 
 			echo '<li class="heading heading-table list-style-none padding-bottom-10">';
-					echo '<div class ="col-md-4">';				_e('JOB TITLE','boxtheme');				echo '</div>';
+					echo '<div class ="col-md-5">';				_e('JOB TITLE','boxtheme');				echo '</div>';
 					echo '<div class ="col-md-2">';				_e('PROPOSALS','boxtheme');				echo '</div>';
-					//echo '<div class ="col-md-1">';				_e('Price','boxtheme');				echo '</div>';
+
 					echo '<div class ="col-md-2">'; _e('DATE POSTED','boxtheme');echo '</div>'; ?>
-					<!-- <div class ="col-md-2 text-center">Status</div> !-->
+					 <div class ="col-md-2 text-center">Status</div> !-->
 					<?php
 					echo '<div class ="col-md-1 text-center pull-right">'; _e('Action','boxtheme');echo '</div>';
 			echo '</li>';
@@ -57,16 +57,15 @@
 					$query->the_post();
 					$project = BX_Project::get_instance()->convert($post);
 					echo '<li class="list-style-none padding-bottom-10">';
-						echo '<div class ="col-md-4">';	echo '<a href="'.get_permalink().'">'. get_the_title().'</a>';	echo '</div>';
+						echo '<div class ="col-md-5">';	echo '<a href="'.get_permalink().'">'. get_the_title().'</a>';	echo '</div>';
 						echo '<div class ="col-md-2">';echo count_bids($post->ID);	echo '</div>';
-						//echo '<div class ="col-md-1">';box_price($project->_budget);echo '</div>';
-						echo '<div class ="col-md-2">';	echo get_the_date();	echo '</div>';
-						?>
-						<!--
+
+						echo '<div class ="col-md-2">';	echo get_the_date();	echo '</div>';						?>
+
 						<div class ="col-md-2 text-center">
-							<?php //echo $project->post_status;?>
+							<?php echo $project->post_status;?>
 						</div>
-						!-->
+
 						<div class ="col-md-1 pull-right text-center">
 							<a href="#" class="btn-board btn-archived-job" id="<?php echo $project->ID;?>"  data-toggle="tooltip" title="<?php printf(__('Archived %s','boxtheme'), $project->post_titile);?>">
 								<i class="fa fa-trash-o" aria-hidden="true"></i>
